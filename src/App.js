aef40582382f036a8bfcc1bed2914c5fc41f4c9f
mehradae <mehradae@gmail.com>
2022-07-20 20:51:08 -0700

    Messed up :D

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import { BiTrash } from "react-icons/bi";
+
 function App() {
   return (
-    <div className="App">
-      <h1>Your Appointments</h1>
+    <div className="App container mx-auto mt-3 font-thin">
+      <h1 className="text-5xl">
+        <BiTrash />
+        Your Appointments
+      </h1>
     </div>
   );
 }
