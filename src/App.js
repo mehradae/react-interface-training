63881ca01162caa368c463b5f8ed9b682461d56f
mehradae <mehradae@gmail.com>
2022-07-26 20:36:36 -0700

    tailwind is working

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import { BiArchive } from "react-icons/bi"
+import { BiArchive } from "react-icons/bi";
 
 function App() {
   return (
-    <div className="App">
-      <h1><BiArchive />Your Appointments</h1>
+    <div className="App container mx-auto mt-3 font-thin">
+      <h1 className="text-5xl">
+        <BiArchive className="inline-block text-red-400" />
+        Your Appointments
+      </h1>
     </div>
   );
 }
